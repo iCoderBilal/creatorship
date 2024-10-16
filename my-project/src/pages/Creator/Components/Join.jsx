c9c6dbc03f66cb965fa269ef8dbb8230a0d4ac16
Mian Bilal <muhammadbilal12345610@gmail.com>
2024-10-17 00:01:58 +0500

    website is responsive

--- my-project/src/pages/Creator/Components/Join.jsx
+++ my-project/src/pages/Creator/Components/Join.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Join = () => {
   return (
     <div className="bg-white p-8">
-      <div className="max-w-7xl mx-auto py-16">
+      <div className="max-w-7xl mx-auto md:py-16 py-16 pt-0">
         <div className="text-left mb-12">
           <h1
             className="text-[9vw] leading-[9.5vw] md:text-[3.5vw] md:leading-[4vw] font-Montserrat font-bold text-black"
@@ -20,9 +20,14 @@ const Join = () => {
         </div>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-16">
           <div className="bg-[#F4F7FA] p-6 shadow-lg rounded-[30px] md:row-span-2 flex justify-center flex-col">
-            <h2 className="text-2xl md:text-4xl font-bold mb-3 font-Montserrat">
-              Partner with Leading Brands
-            </h2>
+            <div className="flex gap-2 items-center justify-center mb-3">
+              <span className="md:text-8xl text-6xl text-[#8649D0] mb-2">
+                ◈
+              </span>
+              <h2 className="text-2xl md:text-3xl font-bold mb-3 font-Montserrat">
+                Partner with Leading Brands
+              </h2>
+            </div>
             <p className="cxobv c6xyh ceonn font-Montserrat text-gray-600 font-medium">
               Get exclusive access to top brands that are eager to hear your
               unique voice. Our partnerships are designed to help you amplify
@@ -30,18 +35,28 @@ const Join = () => {
             </p>
           </div>
           <div className="bg-[#F4F7FA] p-6 shadow-lg rounded-[30px] ">
-            <h2 className="text-2xl font-bold mb-3 font-Montserrat">
-              Turn Your Passion into Earnings
-            </h2>
+            <div className="flex gap-2 items-center justify-center mb-3">
+              <span className="md:text-8xl text-6xl text-[#8649D0] mb-2">
+                ◈
+              </span>
+              <h2 className="text-2xl font-bold mb-3 font-Montserrat ">
+                Turn Your Passion into Earnings
+              </h2>
+            </div>
             <p className="cxobv c6xyh ceonn font-Montserrat text-gray-600 font-medium">
               Earn directly from your creativity. We facilitate collaborations
               and sponsored content that align perfectly with your brand.
             </p>
           </div>
           <div className="bg-[#F4F7FA] p-6 shadow-lg rounded-[30px]  md:row-span-2 flex justify-center flex-col">
-            <h2 className="text-2xl md:text-4xl font-bold mb-3 font-Montserrat">
-              Broaden Your Creative Reach
-            </h2>
+            <div className="flex gap-2 items-center justify-center mb-3">
+              <span className="md:text-8xl text-6xl text-[#8649D0] mb-2">
+                ◈
+              </span>
+              <h2 className="text-2xl md:text-3xl font-bold mb-3 font-Montserrat">
+                Broaden Your Creative Reach
+              </h2>
+            </div>
             <p className="cxobv c6xyh ceonn font-Montserrat text-gray-600 font-medium">
               Step beyond the usual social media activities. With Creatorship,
               you'll find opportunities to endorse products, become a brand
@@ -50,9 +65,14 @@ const Join = () => {
             </p>
           </div>
           <div className="bg-[#F4F7FA] p-6 shadow-lg rounded-[30px]  ">
-            <h2 className="text-2xl font-bold mb-3 font-Montserrat">
-              Enhance Your Skills and Network
-            </h2>
+            <div className="flex gap-2 items-center justify-center mb-3">
+              <span className="md:text-8xl text-6xl text-[#8649D0] mb-2">
+                ◈
+              </span>
+              <h2 className="text-2xl font-bold mb-3 font-Montserrat">
+                Enhance Your Skills and Network
+              </h2>
+            </div>
             <p className="cxobv c6xyh ceonn font-Montserrat text-gray-600 font-medium">
               Access tools and resources from Creatorship to enhance your skills
               and expand your network through targeted workshops and networking
