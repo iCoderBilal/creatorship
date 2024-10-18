0540fc52404bbafb3eeb9fe5721d403965af1931
Mian Bilal <muhammadbilal12345610@gmail.com>
2024-10-18 23:25:44 +0500

    Some Changes have been Made

--- my-project/src/pages/Creator/Components/ExclusiveFeatures.jsx
+++ my-project/src/pages/Creator/Components/ExclusiveFeatures.jsx
@@ -2,54 +2,64 @@ import React from "react";
 
 const ExclusiveFeatures = () => {
   return (
-    <div className="bg-white py-24 max-w-7xl mx-auto">
-      <h2 className="text-center text-2xl font-bold mb-6">
-        Exclusive features
-      </h2>
-      <div className="flex flex-wrap justify-between px-4">
+    <div className="bg-white py-24 sm:pt-24 pt-0 sm:pb-0 lg:pb-8 max-w-7xl mx-auto">
+      <div className="flex flex-wrap justify-between px-4 lg:gap-0 gap-6">
         {/* Feature 1 */}
-        <div className="basis-[48%] w-full shadow-lg overflow-hidden bg-[#401F65] p-4 rounded-[15px]">
-          <div className="exlusive-feature-1 p-6 rounded-[15px]">
+        <div className="lg:basis-[48%] basis-[100%] w-full shadow-lg overflow-hidden bg-[#401F65] p-4 rounded-[15px]">
+          <div className="exlusive-feature-1 lg:p-6 p-4 rounded-[15px] h-full">
             <h1
-              className="mb-12 text-left text-[9vw] leading-[9.5vw] md:text-[2.3vw] md:leading-[2.5vw] font-Montserrat font-bold text-white"
+              className="mb-12 text-left text-2xl md:text-[2.3vw] md:leading-[2.5vw] font-Montserrat font-bold text-white"
               data-aos="fade-up"
               data-aos-delay="100"
             >
-              Share in Success with Equity-Based Partnerships
+              Success With Equity-Based Partnerships
             </h1>
-            <p className="cxobv c6xyh ceonn font-Montserrat text-gray-200 font-medium">
+
+            <p className="cxobv c6xyh ceonn font-Montserrat text-gray-100 font-medium lg:mt-[75px]">
               Our equity-based partnerships mean you share in the success of the
               collaborations you help build. This model ensures that as the
               business grows, so do your earnings and stake in the outcome. It’s
               not just about sponsorship; it’s about building a lasting legacy
               and wealth.
             </p>
-            <button className="bg-white text-brand-purple font-bold py-2 px-4 rounded hover:bg-gray-100">
-              Start Building Wealth Now
-            </button>
+            <div>
+              <a
+                target="_blank"
+                className=" py-3 duration-300 font-bold ease-in-out hover:bg-[#B077EE] font-Montserrat hero-atc ctsgt ctopb c2c8i cwa1c ch1vk c4sti cpi22 cogk8 cmjy8 c38qt c98p9 c4nhw co504 cyhno cn4p0 cjkjb"
+                href="https://calendly.com/jackjay"
+              >
+                Join Our Program
+              </a>
+            </div>
           </div>
         </div>
 
         {/* Feature 2 */}
-        <div className="basis-[48%] w-full shadow-lg overflow-hidden exlusive-feature-1  p-4 rounded-[15px]">
-          <div className=" p-6 rounded-[15px] bg-[#F4F7FA]">
+        <div className="lg:basis-[48%] basis-[100%] w-full shadow-lg overflow-hidden exlusive-feature-1  p-4 rounded-[15px]">
+          <div className=" lg:p-6 p-4 rounded-[15px] bg-[#F4F7FA] h-full">
             <h1
-              className="mb-12 text-left text-[9vw] leading-[9.5vw] md:text-[2.3vw] md:leading-[2.5vw] font-Montserrat font-bold text-black"
+              className="mb-12 text-left text-2xl md:text-[2.3vw] md:leading-[2.5vw] font-Montserrat font-bold text-black"
               data-aos="fade-up"
               data-aos-delay="100"
             >
-              Share in Success with Equity-Based Partnerships
+              Building Brands from Scratch
             </h1>
             <p className="cxobv c6xyh ceonn font-Montserrat text-gray-600 font-medium">
-              Our equity-based partnerships mean you share in the success of the
-              collaborations you help build. This model ensures that as the
-              business grows, so do your earnings and stake in the outcome. It’s
-              not just about sponsorship; it’s about building a lasting legacy
-              and wealth.
+              Ever dreamed of having your own brand? We make it a reality. At
+              Creatorship, we specialize in building brands from scratch,
+              tailored to your vision and voice. Our team supports you every
+              step of the way, from initial concept to market launch, ensuring
+              you have a professional, market-ready brand that stands out.
             </p>
-            <button className="bg-white text-brand-purple font-bold py-2 px-4 rounded hover:bg-gray-100">
-              Start Building Wealth Now
-            </button>
+            <div>
+              <a
+                target="_blank"
+                className="bg-[#8649D0] text-white py-3  duration-300 ease-in-out hover:bg-[#643A8F] font-Montserrat hero-atc ctsgt ctopb c2c8i cwa1c ch1vk c4sti cpi22 cogk8 cmjy8 c38qt c98p9 c4nhw co504 cyhno cn4p0 cjkjb"
+                href="https://calendly.com/jackjay"
+              >
+                Join Our Program
+              </a>
+            </div>
           </div>
         </div>
       </div>
